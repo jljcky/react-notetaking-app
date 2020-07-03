50068a72043e5d60ab937713178813fc42f806ce
unknown <lojacky123@gmail.com>
2020-07-03 12:13:22 -0400

    Updates

---
+++ precache-manifest.0778deb18255df8c281b9670bf34838d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0031bf054091beba8c6ce23742627a66",
+    "url": "/react-notetaking-app/index.html"
+  },
+  {
+    "revision": "f9ee1baf750d69070eb1",
+    "url": "/react-notetaking-app/static/css/main.b3b8edc7.chunk.css"
+  },
+  {
+    "revision": "94811af00404cd4038d7",
+    "url": "/react-notetaking-app/static/js/2.3deffe65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-notetaking-app/static/js/2.3deffe65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ee1baf750d69070eb1",
+    "url": "/react-notetaking-app/static/js/main.ab5a80f1.chunk.js"
+  },
+  {
+    "revision": "6db8e6fab86e68806bd9",
+    "url": "/react-notetaking-app/static/js/runtime-main.d79b33a7.js"
+  }
+]);
